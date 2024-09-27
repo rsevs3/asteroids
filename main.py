d2a2f2803c0357d138b021ae7d7f3b2f8ee62502
Ryan Sevelj <2338952+rsevs3@users.noreply.github.com>
2024-09-28 00:16:02 +0930

    Got the player drawing on the screen

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from player import Player
 import pygame
 
 from constants import *
@@ -5,13 +6,18 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000000)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
