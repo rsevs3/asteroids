d74e3d677d05129eec36c32ec7280816d72318a3
Ryan Sevelj <2338952+rsevs3@users.noreply.github.com>
2024-09-28 00:38:14 +0930

    Added Player to groups and do the updates from the groups

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
@@ -15,8 +18,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for member in updatable:
+            member.update(dt)
+        for member in drawable:
+            member.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
